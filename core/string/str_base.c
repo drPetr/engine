09f39316ffc971243d57f11672f1ecf7b5530d98
drpetr <petr.timohovich@gmail.com>
2020-02-20 17:18:41 +0300

    update string functions

--- core/string/str_base.c
+++ core/string/str_base.c
@@ -2,3 +2,157 @@
 
 #include <core/memory.h>
 #include <core/assert.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+
+/*
+============
+StrCreate
+============
+*/
+char* StrCreate( const char* s ) {
+    char* ret;
+    assert( s );
+    ret = MemStringAlloc()->alloc( StrLen( s ) + 1 );
+    StrCpy( ret, s );
+    return ret;
+}
+
+/*
+============
+StrDelete
+============
+*/
+void StrDelete( char* s ) {
+    assert( s );
+    MemStringAlloc()->dealloc( s );
+}
+
+/*
+============
+StrCmp
+============
+*/
+int StrCmp( const char* s1, const char* s2 ) {
+    return strcmp( s1, s2 );
+}
+
+/*
+============
+StrCpy
+============
+*/
+char* StrCpy( char* dst, const char* src ) {
+    return strcpy( dst, src );
+}
+
+/*
+============
+StrLen
+============
+*/
+size_t StrLen( const char* str ) {
+    return strlen( str );
+}
+
+/*
+============
+IsNumeric
+============
+*/
+bool_t IsNumeric( const char *s ) {
+	if ( *s == '-' ) {
+		s++;
+	}
+    
+    if( isdigit(*s) ) {
+        s++;
+        while( isdigit( *s ) ) {
+            s++;
+        }
+        // check for dot
+        if( *s && *s == '.' ) {
+            s++;
+        }
+        while( isdigit( *s ) ) {
+            s++;
+        }
+    } else {
+        return bfalse;
+    }
+
+    return !*s;
+}
+
+/*
+============
+StrIsInt
+============
+*/
+bool_t StrIsInt( const char* s ) {
+    if ( *s == '-' ) {
+		s++;
+	}
+    
+    if( isdigit(*s) ) {
+        s++;
+        while( isdigit( *s ) ) {
+            s++;
+        }
+    } else {
+        return bfalse;
+    }
+    
+    return !*s;
+}
+
+/*
+============
+StrIsFloat
+============
+*/
+bool_t StrIsFloat( const char* s ) {
+    if ( *s == '-' ) {
+		s++;
+	}
+    
+    if( isdigit(*s) ) {
+        s++;
+        while( isdigit( *s ) ) {
+            s++;
+        }
+        // check for dot
+        if( *s && *s == '.' ) {
+            s++;
+        } else {
+            return bfalse;
+        }
+        while( isdigit( *s ) ) {
+            s++;
+        }
+    } else {
+        return bfalse;
+    }
+
+    return !*s;
+}
+
+/*
+============
+StrToInt
+============
+*/
+int StrToInt( const char* s ) {
+    return atoi( s );
+}
+
+/*
+============
+StrToFloat
+============
+*/
+float StrToFloat( const char* s ) {
+    return atof( s );
+}
--- core/string/str_base.h
+++ core/string/str_base.h
@@ -4,5 +4,36 @@
 #include <core/types.h>
 
 
+char*   StrCreate( const char* s );
+// allocate memory for string and copy string
+
+void    StrDelete( char* s );
+// deallocate string memory and delete string
+
+int     StrCmp( const char* s1, const char* s2 );
+// compare two string and return -1 if s1 is less than s2,
+// or 1 if s1 is more than s2, or 0 if s1 is equal to s2
+
+char*   StrCpy( char* dst, const char* src );
+// copy string from src to dst
+
+size_t  StrLen( const char* str );
+// return length of the string
+
+bool_t  StrIsNumeric( const char* s );
+// return btrue if str is int value or float value
+
+bool_t  StrIsInt( const char* s );
+// return btrue if str is int value
+
+bool_t  StrIsFloat( const char* s );
+// return bture if str is float value
+
+int     StrToInt( const char* s );
+// parse integer value
+
+float   StrToFloat( const char* s );
+// parse float value
+
 
 #endif //__STR_BASE_H__
