e1aeb3786db1d150b993a39f2c1f663ae48c1c0f
drpetr <petr.timohovich@gmail.com>
2020-02-20 15:15:27 +0300

    add string functions

---
+++ core/string/str_base.c
@@ -0,0 +1,4 @@
+#include "str_base.h"
+
+#include <core/memory.h>
+#include <core/assert.h>
---
+++ core/string/str_base.h
@@ -0,0 +1,8 @@
+#ifndef __STR_BASE_H__
+#define __STR_BASE_H__
+
+#include <core/types.h>
+
+
+
+#endif //__STR_BASE_H__
