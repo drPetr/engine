4576f1079e7e3c99a8907152736cd9a6c967f480
drpetr <petr.timohovich@gmail.com>
2020-02-24 18:11:46 +0300

    'var' functional update

--- core/var/var.h
+++ core/var/var.h
@@ -12,19 +12,31 @@ VAR - global variable system
 */
 
 enum varFlags_t {
-    VAR_TYPE_INT = 1,
-    VAR_TYPE_FLOAT = 2,
-    VAR_TYPE_STRING = 3,
+    VAR_INT = 1,
+    VAR_FLT = 2,
+    VAR_STR = 3,
     VAR_TYPE_MASK = 0x0f
 };
 
 typedef struct var_t {
-    char*           name;
-    char*           descr;
-    char*           s;
-    int             i;
-    float           f;
-    uint32_t        flags;
+    char*           name;       // var name
+    char*           descr;      // description
+    char*           lim;        // limits
+    uint32_t        flags;      // flags
+    uint32_t        upd;        // last update counter
+    union {
+        struct {
+            int     i;          // int value
+            int     imin;       // min int value
+            int     imax;       // max int value
+        };
+        struct {
+            float   f;          // float value
+            float   fmin;       // min float value
+            float   fmax;       // max float value
+        };
+        char*       s;          // string value
+    };
 } var_t;
 
 
@@ -32,11 +44,13 @@ typedef struct var_t {
 void            VarInit( void );
 void            VarShutdown( void );
 
-
-var_t*          VarCreate( const char* name, const char* val, uint32_t flags );
-var_t*          VarSet( const char* name, const char* val );
-var_t*          VarGet( const char* name );
-bool_t          VarDelete( const char* name );
+var_t*          VarCreate( const char* name, uint32_t flags, const char* val, const char* lim, const char* descr );
+var_t*          VarFind( const char* name );
+uint32_t        VarType( const var_t* v );
+bool_t          VarUpdate( var_t* v, const char* val );
+bool_t          VarUpdateLim( var_t* v, const char* lim );
+bool_t          VarUpdateDescr( var_t* v, const char* descr );
+bool_t          VarDelete( var_t* v );
 
 
 #endif //__base_VAR_H__
