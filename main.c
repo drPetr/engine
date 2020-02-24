49d5b7b1c9c27fe4122974b429984cb59fecaba0
drpetr <petr.timohovich@gmail.com>
2020-02-24 18:12:26 +0300

    update main.c file

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+#include <core/var.h>
+#include <core/string.h>
+
 int main( int argc, char** argv ) {
+    const char s[] = "  -2147483648    asdf";
+    const char* end = NULL;
+    int i = 0;
     
-   
+    printf( "%d\n", StrToInt( s, &end, NULL ) );
+    printf( "%s\n", end );
+    printf( "%d\n", i );
+
     return 0;
 }
