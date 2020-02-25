6382fec0f54ed13ddf9f6d93e0802e730040eb38
drpetr <petr.timohovich@gmail.com>
2020-02-25 16:46:39 +0300

    update main.c

--- main.c
+++ main.c
@@ -3,14 +3,22 @@
 #include <core/var.h>
 #include <core/string.h>
 
-int main( int argc, char** argv ) {
-    const char s[] = "  -2147483648    asdf";
-    const char* end = NULL;
-    int i = 0;
-    
-    printf( "%d\n", StrToInt( s, &end, NULL ) );
-    printf( "%s\n", end );
-    printf( "%d\n", i );
+var_t* s_vol;
+var_t* s_mouse;
 
+int main( int argc, char** argv ) {
+    VarInit();
+    char scl[] = "warning";
+
+    s_vol = VarCreate( "s_vol", VAR_FLT, "0.8", "0;100", "sound volume" );
+    s_mouse = VarCreate( "s_mouse", VAR_FLT, "0.5", "0;1", "sound volume" );
+
+    VarUpdate( s_vol, "22.412323" );
+
+    printf( "name: %s; val: %f [%f;%f]\n", s_vol->name, s_vol->f, s_vol->fmin, s_vol->fmax );
+    printf( "name: %s; val: %f [%f;%f]\n", s_mouse->name, s_mouse->f, s_mouse->fmin, s_mouse->fmax );
+    //printf( "%d\n", s_vol->upd );
+
+    VarShutdown();
     return 0;
 }
