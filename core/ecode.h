94c74993425d4352b9203da5d8f52821257cbc10
drpetr <petr.timohovich@gmail.com>
2020-03-06 16:48:51 +0300

    update ecodes

--- core/ecode.h
+++ core/ecode.h
@@ -8,7 +8,10 @@ typedef enum {
     E_NUMRNG,           // number out of range
     E_NOTNUM,           // is not number
     E_NODATA,           // no data
+    E_BADF              // bad file descriptor
 } ecode_t;
+
+#define OK      E_OK
 
 
 #endif //__ECODE_H__
