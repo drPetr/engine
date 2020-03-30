c85c046739b4ef90ee103c2933a11184d40c64e3
drpetr <petr.timohovich@gmail.com>
2020-03-30 16:28:45 +0300

    add tree structure

--- core/containers/tree.c
+++ core/containers/tree.c
@@ -2,11 +2,11 @@
 #include <core/assert.h>
 
 /*
-====================
+============
 TreeInsertionHelper
-====================
+============
 */
-static void TreeInsertionHelper( tree_t* left, tree_t* right, tree_t* parent, tree_t* insert ) {
+static tree_t* TreeInsertionHelper( tree_t* left, tree_t* right, tree_t* parent, tree_t* insert ) {
     // first node in the insert sequence
     tree_t* firstInsert = insert;
     // last node in the insert sequence
@@ -52,105 +52,318 @@ static void TreeInsertionHelper( tree_t* left, tree_t* right, tree_t* parent, tr
 			lastInsert->next = right;
 		}
     }
+    return firstInsert;
 }
 
-
-
 /*
-====================
-TreeNodeAllocate
-====================
+============
+TreeExtractionHelper
+============
 */
-tree_t* TreeNodeAllocate( alloc_t* alloc, size_t subsize ) {
-    tree_t* node;
-    node = alloc->alloc( sizeof(tree_t) + subsize );
-    node->parent = NULL;
-	node->next = NULL;
-	node->prev = NULL;
-	node->firstChild = NULL;
-	node->lastChild = NULL;
-    node->flags = 0;
+static tree_t* TreeExtractionHelper( tree_t* left, tree_t* right ) {
+    tree_t* ext = NULL;
+    tree_t* it;
+    tree_t* parent;
+    tree_t* before;
+    tree_t* after;
     
-    return node;
+    // should  be at least one node
+    assert( left || right );
+    
+    // if left node is set and right node is set
+    if( left && right ) {
+        ext = left;
+#ifdef DEBUG
+        // check left and right sequence
+        if( ext != right ) {
+            while( ext && ext != right ) {
+                ext = ext->next;
+            }
+        }
+        asserta( ext == right, "error: incorrect order of parameters" );
+        ext = left;
+#endif
+    } else if( left || right ) { // if only one node is set
+        // if only one left node
+        if( left ) {
+            ext = left;
+            right = left;
+            while( right->next ) {
+                right = right->next;
+            }
+        } else { // else right node
+            left = right;
+            while( left->prev ) {
+                left = left->prev;
+            }
+            ext = left;
+        }
+    }
+    
+    parent = left->parent;
+    before = left->prev;
+    after = right->next;
+    
+    // if the left node has a previous sibling and
+    // the right node has a next sibling
+    if( before &&after ) {
+        before->next = after;
+        after->prev = before;
+        left->prev = NULL;
+        right->next = NULL;
+    } else if( before || after ) { // else only one node has a sibling
+        // if the left node has a previous sibling and
+        if( before ) {
+            before->next = NULL;
+            left->prev = NULL;
+        } else { // else the right node has a next sibling
+            after->prev = NULL;
+            right->next = NULL;
+        }
+    }
+    
+    // disconnect parents
+    if( parent ) {
+        if( left == parent->firstChild && right == parent->lastChild ) {
+            // no descendants
+            parent->firstChild = NULL;
+            parent->lastChild = NULL;
+        } else if( left == parent->firstChild || right == parent->lastChild ) {
+            // set new pointers to descendants
+            if( left == parent->firstChild ) {
+                parent->firstChild = after;
+            } else {
+                parent->lastChild = before;
+            }
+        }
+        
+        it = left;
+        do {
+            it->parent = NULL;
+            it = it->next;
+        } while( it != right );
+    }
+    
+    return ext;
 }
 
 /*
 ============
-TreeNodeDeallocate
+TreePreOrder_r
 ============
 */
-void TreeNodeDeallocate( alloc_t* alloc, tree_t* node ) {
-    alloc->dealloc( node );
+static int TreePreOrder_r( tree_t* node, fnTreeOrder order, void* userData ) {
+    int code;
+    
+    // exit if NULL
+    if( !node ) {
+        return 0;
+    }
+    
+    // call user function
+    if( (code = order( node->data, userData )) ) {
+        return code;
+    }
+    // recursive call for first child
+    if( (code = TreePreOrder_r( node->firstChild, order, userData )) ) {
+        return code;
+    }
+    // recursive call for next node
+    if( (code = TreePreOrder_r( node->next, order, userData )) ) {
+        return code;
+    }
+    
+    return 0;
 }
 
 /*
-====================
-TreeInsertFirstChild
-
-Вставить узел insert первым дочерним в node
-====================
+============
+TreeInOrder_r
+============
 */
-void TreeInsertFirstChild( tree_t* node, tree_t* insert ) {
-    TreeInsertionHelper( NULL, node->firstChild, node, insert );
+static int TreeInOrder_r( tree_t* node, fnTreeOrder order, void* userData ) {
+    int code;
+    
+    // exit if NULL
+    if( !node ) {
+        return 0;
+    }
+    
+    // recursive call for first child
+    if( (code = TreeInOrder_r( node->firstChild, order, userData )) ) {
+        return code;
+    }
+    // call user function
+    if( (code = order( node->data, userData )) ) {
+        return code;
+    }
+    // recursive call for next node
+    if( (code = TreeInOrder_r( node->next, order, userData )) ) {
+        return code;
+    }
+    
+    return 0;
 }
 
 /*
-====================
-TreeInsertLastChild
-
-Вставить узел insert последним дочерним в node
-====================
+============
+TreePostOrder_r
+============
 */
-void TreeInsertLastChild( tree_t* node, tree_t* insert ) {
-    TreeInsertionHelper( node->lastChild, NULL, node, insert );
+static int TreePostOrder_r( tree_t* node, fnTreeOrder order, void* userData ) {
+    int code;
+    
+    // exit if NULL
+    if( !node ) {
+        return 0;
+    }
+    
+    // recursive call for first child
+    if( (code = TreePostOrder_r( node->firstChild, order, userData )) ) {
+        return code;
+    }
+    // recursive call for next node
+    if( (code = TreePostOrder_r( node->next, order, userData )) ) {
+        return code;
+    }
+    // call user function
+    if( (code = order( node->data, userData )) ) {
+        return code;
+    }
+    
+    return 0;
 }
 
 /*
-====================
-TreeInsertBefore
-
-Вставить узел insert перед узлом node
-====================
+============
+_TreeInsertFirstChild
+============
 */
-void TreeInsertBefore( tree_t* node, tree_t* insert ) {
-    TreeInsertionHelper( node->prev, node, node->parent, insert );
+tree_t* _TreeInsertFirstChild( tree_t* node, tree_t* insert ) {
+    return TreeInsertionHelper( NULL, node->firstChild, node, insert );
 }
 
 /*
-====================
-TreeInsertAfter
-
-Вставить узел insert после узла node
-====================
+============
+_TreeInsertLastChild
+============
 */
-void TreeInsertAfter( tree_t* node, tree_t* insert ) {
-    TreeInsertionHelper( node, node->next, node->parent, insert );
+tree_t* _TreeInsertLastChild( tree_t* node, tree_t* insert ) {
+    return TreeInsertionHelper( node->lastChild, NULL, node, insert );
 }
 
 /*
-====================
-TreeInsertFirst
-
-Вставить узел insert первым в последовательности node
-====================
+============
+_TreeInsertBefore
+============
 */
-void TreeInsertFirst( tree_t* node, tree_t* insert ) {
+tree_t* _TreeInsertBefore( tree_t* node, tree_t* insert ) {
+    return TreeInsertionHelper( node->prev, node, node->parent, insert );
+}
+
+/*
+============
+_TreeInsertAfter
+============
+*/
+tree_t* _TreeInsertAfter( tree_t* node, tree_t* insert ) {
+    return TreeInsertionHelper( node, node->next, node->parent, insert );
+}
+
+/*
+============
+_TreeInsertFirst
+============
+*/
+tree_t* _TreeInsertFirst( tree_t* node, tree_t* insert ) {
     while( node->prev ) {
         node = node->prev;
     }
-    TreeInsertionHelper( NULL, node, node->parent, insert );
+    return TreeInsertionHelper( NULL, node, node->parent, insert );
 }
 
 /*
-====================
-TreeInsertLast
-
-Вставить узел insert последним в последовательности node
-====================
+============
+_TreeInsertLast
+============
 */
-void TreeInsertLast( tree_t* node, tree_t* insert ) {
+tree_t* _TreeInsertLast( tree_t* node, tree_t* insert ) {
     while( node->next ) {
         node = node->next;
     }
-    TreeInsertionHelper( node, NULL, node->parent, insert );
+    return TreeInsertionHelper( node, NULL, node->parent, insert );
+}
+
+/*
+============
+_TreeExtract
+============
+*/
+tree_t* _TreeExtract( tree_t* node ) {
+    return TreeExtractionHelper( node, node );
+}
+
+/*
+============
+_TreeExtractSiblings
+============
+*/
+tree_t* _TreeExtractSiblings( tree_t* left, tree_t* right ) {
+    return TreeExtractionHelper( left, right );
+}
+
+/*
+============
+TreeExtractChildren
+============
+*/
+tree_t* _TreeExtractChildren( tree_t* node ) {
+    if( node && node->firstChild ) {
+        return TreeExtractionHelper( node->firstChild, node->lastChild );
+    }
+    return NULL;
+}
+
+/*
+============
+_TreePreOrder
+============
+*/
+int _TreePreOrder( tree_t* node, fnTreeOrder order, void* userData ) {
+    assert_not_null( order );
+    return TreePreOrder_r( node, order, userData );
+}
+
+/*
+============
+_TreeInOrder
+============
+*/
+int _TreeInOrder( tree_t* node, fnTreeOrder order, void* userData ) {
+    assert_not_null( order );
+    return TreeInOrder_r( node, order, userData );
+}
+
+/*
+============
+_TreePostOrder
+============
+*/
+int _TreePostOrder( tree_t* node, fnTreeOrder order, void* userData ) {
+    assert_not_null( order );
+    return TreePostOrder_r( node, order, userData );
+}
+
+/*
+============
+_TreeNodeSetToNULL
+============
+*/
+void _TreeNodeSetToNULL( tree_t* node ) {
+    assert_not_null( node );
+    node->parent = NULL;
+    node->prev = NULL;
+    node->next = NULL;
+    node->firstChild = NULL;
+    node->lastChild = NULL;
 }
--- core/tree.h
+++ core/tree.h
@@ -2,28 +2,100 @@
 #define __TREE_H__
 
 #include <core/types.h>
-#include <core/memory.h>
 
+
+
+//                  data ptr, user data ptr
+typedef int(*fnTreeOrder)(void*,void*);
+
+// tree
 typedef struct tree_t {
     struct tree_t*      parent;
-    struct tree_t*      next;
     struct tree_t*      prev;
+    struct tree_t*      next;
     struct tree_t*      firstChild;
     struct tree_t*      lastChild;
-    uint32_t            flags;
     uint8_t             data[0];
 } tree_t;
 
+#define TREE_DATA_OFFSET    ((ssize_t)(sizeof(tree_t)))
 
 
-tree_t* TreeNodeAllocate( alloc_t* alloc, size_t subsize );
-void    TreeNodeDeallocate( alloc_t* alloc, tree_t* node );
-void    TreeInsertFirstChild( tree_t* node, tree_t* insert );
-void    TreeInsertLastChild( tree_t* node, tree_t* insert );
-void    TreeInsertBefore( tree_t* node, tree_t* insert );
-void    TreeInsertAfter( tree_t* node, tree_t* insert );
-void    TreeInsertFirst( tree_t* node, tree_t* insert );
-void    TreeInsertLast( tree_t* node, tree_t* insert );
+
+tree_t*     _TreeInsertFirstChild( tree_t* node, tree_t* insert );
+// insert a subtree into the first child of tree
+// function returns a pointer to the first inserted node
+
+tree_t*     _TreeInsertLastChild( tree_t* node, tree_t* insert );
+// insert a subtree into the last child of tree
+// function returns a pointer to the first inserted node
+
+tree_t*     _TreeInsertBefore( tree_t* node, tree_t* insert );
+// insert a subtree before node
+// function returns a pointer to the first inserted node
+
+tree_t*     _TreeInsertAfter( tree_t* node, tree_t* insert );
+// insert a subtree after node
+// function returns a pointer to the first inserted node
+
+tree_t*     _TreeInsertFirst( tree_t* node, tree_t* insert );
+// insert a subtree to first
+// function returns a pointer to the first inserted node
+
+tree_t*     _TreeInsertLast( tree_t* node, tree_t* insert );
+// insert a subtree to last
+// function returns a pointer to the first inserted node
+
+tree_t*     _TreeExtract( tree_t* node );
+// extract a subtree from a tree
+// function returns a pointer to the extracted node
+
+tree_t*     _TreeExtractSiblings( tree_t* left, tree_t* right );
+// extract sequence of siblings from a tree
+// function returns a pointer to the extracted node
+
+tree_t*     _TreeExtractChildren( tree_t* node );
+// extract sequence of child
+// function returns a pointer to the extracted node
+
+int         _TreePreOrder( tree_t* node, fnTreeOrder order, void* userData );
+// pre-order tree traversal
+// function returns number from callback of function the order
+
+int         _TreeInOrder( tree_t* node, fnTreeOrder order, void* userData );
+// in-order tree traversal
+// function returns number from callback of function the order
+
+int         _TreePostOrder( tree_t* node, fnTreeOrder order, void* userData );
+// post-order tree traversal
+// function returns number from callback of function the order
+
+void        _TreeNodeSetToNULL( tree_t* node );
+// set all pointers to NULL
+
+
+
+// aliases
+#define TreeInsertFirstChild(n,i)   ((void*)_TreeInsertFirstChild(((tree_t*)(void*)(n)),(tree_t*)(void*)(i)))
+#define TreeInsertLastChild(n,i)    ((void*)_TreeInsertLastChild(((tree_t*)(void*)(n)),(tree_t*)(void*)(i)))
+#define TreeInsertBefore(n,i)       ((void*)_TreeInsertBefore(((tree_t*)(void*)(n)),(tree_t*)(void*)(i)))
+#define TreeInsertAfter(n,i)        ((void*)_TreeInsertAfter(((tree_t*)(void*)(n)),(tree_t*)(void*)(i)))
+#define TreeInsertFirst(n,i)        ((void*)_TreeInsertFirst(((tree_t*)(void*)(n)),(tree_t*)(void*)(i)))
+#define TreeInsertLast(n,i)         ((void*)_TreeInsertLast(((tree_t*)(void*)(n)),(tree_t*)(void*)(i)))
+#define TreeExtract(n)              ((void*)_TreeExtract((n)))
+#define TreeExtractSiblings(l,r)    ((void*)_TreeExtractSiblings((l),(r)))
+#define TreeExtractChildren(n)      ((void*)_TreeExtractChildren((n)))
+#define TreePreOrder(n,o,u)         (_TreePreOrder((tree_t*)(void*)(n),o,(void*)(u)))
+#define TreeInOrder(n,o,u)          (_TreeInOrder((tree_t*)(void*)(n),o,(void*)(u)))
+#define TreePostOrder(n,o,u)        (_TreePostOrder((tree_t*)(void*)(n),o,(void*)(u)))
+#define TreeNodeSetToNULL(n)        (_TreeNodeSetToNULL(((tree_t*)(void*)(n))))
+
+#define TreeNext(n)                 ((void*)(((tree_t*)(void*)(n))->next))
+#define TreePrev(n)                 ((void*)(((tree_t*)(void*)(n))->prev))
+#define TreeFirstChild(n)           ((void*)(((tree_t*)(void*)(n))->firstChild))
+#define TreeLastChild(n)            ((void*)(((tree_t*)(void*)(n))->lastChild))
+#define TreeParent(n)               ((void*)(((tree_t*)(void*)(n))->parent))
+
 
 
 #endif //__TREE_H__
